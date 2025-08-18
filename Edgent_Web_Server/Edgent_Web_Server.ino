d56d1932e7591b9ce58f76d859a5ff8bfc542c8b
prasannan-robots <prasannanatarajan3595@gmail.com>
2025-08-18 09:03:13 +0530

    First

---
+++ Edgent_Web_Server/Edgent_Web_Server.ino
@@ -0,0 +1,429 @@
+// #endif
+#include "esp_camera.h"
+#include <WiFi.h>
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+//            You must select partition scheme from the board menu that has at least 3MB APP space.
+//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15
+//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
+
+// ===================
+// Select camera model
+// ===================
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE  // Has PSRAM
+//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_CAMS3_UNIT  // Has PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+//#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+// ** Espressif Internal Boards **
+//#define CAMERA_MODEL_ESP32_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S3_CAM_LCD
+//#define CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3 // Has PSRAM
+//#define CAMERA_MODEL_DFRobot_Romeo_ESP32S3 // Has PSRAM
+#define BLYNK_TEMPLATE_ID "TMPL3TWSW8w4R"
+#define BLYNK_TEMPLATE_NAME "Smart Camera"
+#define BLYNK_FIRMWARE_VERSION        "0.1.2"
+
+#define BLYNK_PRINT Serial
+#define BLYNK_DEBUG
+#define APP_DEBUG
+#include <WiFi.h>
+#include "BlynkEdgent.h"
+#include "camera_pins.h"
+//#include <PubSubClient.h>
+//#include <WiFiClientSecure.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include <Prashant_Singh-project-1_inferencing.h>
+#include "edge-impulse-sdk/dsp/image/image.hpp"
+#define EI_CAMERA_RAW_FRAME_BUFFER_COLS           320
+#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS           240
+#define EI_CAMERA_FRAME_BYTE_SIZE                 3
+#define MSG_BUFFER_SIZE (500)
+#define BLYNK_TEMPLATE_ID "TMPL3TWSW8w4R"
+#define BLYNK_TEMPLATE_NAME "Smart Camera"
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+const char *ssid = "natarajanv";
+const char *password = "prasanna@05";
+// const char* mqtt_server = "a7cb0fef114e49ce9263c516dd6f7e61.s1.eu.hivemq.cloud"; // replace with your HiveMQ Cluster URL
+// const char* mqtt_username = "ESP32Client"; // replace with your Username
+// const char* mqtt_password = "Summathan1"; // replace with your Password
+// const int mqtt_port = 8883;
+uint8_t *snapshot_buf; //points to the output of the capture
+SemaphoreHandle_t fb_mutex = NULL;
+size_t out_len = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+bool do_resize = false;
+static bool debug_nn = false;
+unsigned long lastDetectionTime = 0;
+bool detectionReported = false;
+const unsigned long DETECTION_TIMEOUT = 15000; 
+int detection = 0;
+char msg[MSG_BUFFER_SIZE];
+//WiFiClientSecure espClient;  
+//PubSubClient client(espClient);
+QueueHandle_t frame_queue;
+
+
+// HiveMQ Cloud Let's Encrypt CA certificate
+// static const char *root_ca PROGMEM = R"EOF(
+// -----BEGIN CERTIFICATE-----
+// MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+// TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+// cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+// WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+// ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+// MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+// h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+// 0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+// A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+// T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+// B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+// B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+// KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+// OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+// jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+// qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+// rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+// HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+// hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+// ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+// 3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+// NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+// ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+// TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+// jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+// oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+// 4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+// mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+// emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+// -----END CERTIFICATE-----
+// )EOF";
+
+// void publish_client(char msg[]){
+//   client.publish("SensePresence", msg);
+// }
+
+
+// void reconnect() {
+//   // Loop until we’re reconnected
+//   while (!client.connected()) {
+//     Serial.print("Attempting MQTT connection… ");
+//     String clientId = "ESP32Client";
+//     // Attempt to connect
+//     if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+//       Serial.println("connected!");
+//       // Once connected, publish an announcement…
+//       client.publish("online", "Sensor v1 online");
+     
+//     } else {
+//       Serial.print("failed, rc = ");
+//       Serial.print(client.state());
+//       Serial.println(" try again in 5 seconds");
+//       // Wait 5 seconds before retrying
+//       delay(5000);
+//     }
+//   }
+// }
+
+
+static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    // we already have a RGB888 buffer, so recalculate offset into pixel index
+    size_t pixel_ix = offset * 3;
+    size_t pixels_left = length;
+    size_t out_ptr_ix = 0;
+
+    while (pixels_left != 0) {
+        // Swap BGR to RGB here
+        // due to https://github.com/espressif/esp32-camera/issues/379
+        out_ptr[out_ptr_ix] = (snapshot_buf[pixel_ix + 2] << 16) + (snapshot_buf[pixel_ix + 1] << 8) + snapshot_buf[pixel_ix];
+
+        // go to the next pixel
+        out_ptr_ix++;
+        pixel_ix+=3;
+        pixels_left--;
+    }
+    // and done!
+    return 0;
+}
+void startCameraServer();
+void setupLedFlash(int pin);
+BLYNK_WRITE(V1) { detection = param.asInt();}
+void blynk_task(void *pvParameters){
+  while (1){
+  BlynkEdgent.run();
+  // if (!client.connected()) {
+  //   reconnect();
+  // }
+  // client.loop();
+ int temp = temperatureRead();  // Convert to °C properly
+ Blynk.virtualWrite(V3, temp); 
+  // Do nothing. Everything is done in another task by the web server
+  vTaskDelay(pdMS_TO_TICKS(10000));
+  }
+}
+void classifier_task(void *pvParameters) {
+  
+
+    while (1) {
+      Blynk.syncVirtual(V1);
+      Serial.println(detection);
+       if (xQueueReceive(frame_queue, &snapshot_buf, portMAX_DELAY) == pdTRUE && detection==1) {
+    xSemaphoreTake(fb_mutex, portMAX_DELAY);
+    ei::signal_t signal;
+    signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+    signal.get_data = &ei_camera_get_data;
+
+   
+       if ((EI_CLASSIFIER_INPUT_WIDTH != EI_CAMERA_RAW_FRAME_BUFFER_COLS)
+           || (EI_CLASSIFIER_INPUT_HEIGHT != EI_CAMERA_RAW_FRAME_BUFFER_ROWS)) {
+           do_resize = true;
+       }
+   
+       if (do_resize) {
+           ei::image::processing::crop_and_interpolate_rgb888(
+            snapshot_buf,
+           EI_CAMERA_RAW_FRAME_BUFFER_COLS,
+           EI_CAMERA_RAW_FRAME_BUFFER_ROWS,
+           snapshot_buf,
+           EI_CLASSIFIER_INPUT_WIDTH,
+           EI_CLASSIFIER_INPUT_HEIGHT);
+       }
+        ei_impulse_result_t result = { 0 };
+
+    EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+        Serial.print(err);
+        //res = ESP_FAIL;
+    }
+     xSemaphoreGive(fb_mutex);
+
+    // print the predictions
+   log_e("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+                result.timing.dsp, result.timing.classification, result.timing.anomaly);
+Serial.printf("Running on core: %d\n", xPortGetCoreID());
+
+#if EI_CLASSIFIER_OBJECT_DETECTION == 1
+    Serial.print("Object detection bounding boxes:\r\n");
+    bool objectDetected = false;
+    for (uint32_t i = 0; i < result.bounding_boxes_count; i++) {
+        ei_impulse_result_bounding_box_t bb = result.bounding_boxes[i];
+        if (bb.value == 0) {
+            continue;
+        }
+          objectDetected = true;
+        char buffer[128];
+snprintf(buffer, sizeof(buffer), "  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]",
+         bb.label,
+         bb.value,
+         bb.x,
+         bb.y,
+         bb.width,
+         bb.height);
+Serial.println(buffer);
+// snprintf (msg, MSG_BUFFER_SIZE, "Detected");
+// publish_client(msg);
+Blynk.virtualWrite(V2,1);
+    }
+    if (objectDetected) {
+        lastDetectionTime = millis();
+        detectionReported = false;
+    } 
+    // Check if we need to send "Not detected" message
+    else if (!detectionReported && millis() - lastDetectionTime > DETECTION_TIMEOUT) {
+      //  snprintf(msg, MSG_BUFFER_SIZE, "Not detected");
+     //   publish_client(msg);
+
+        Blynk.virtualWrite(V2,0);
+        detectionReported = true; // Set flag to avoid sending repeatedly
+        Serial.println("No detection for 1 minute, sent 'Not detected' message");
+    }
+
+    // Print the prediction results (classification)
+#else
+    Serial.print("Predictions:\r\n");
+    for (uint16_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
+        log_e("  %s: ", ei_classifier_inferencing_categories[i]);
+       log_e("%.5f\r\n", result.classification[i].value);
+    }
+#endif
+//}
+      
+       vTaskDelay(5000); }}
+}
+
+void setup() {
+  pinMode(33,OUTPUT);
+  digitalWrite(33,HIGH);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+    BlynkEdgent.begin();
+    while (!Blynk.connected()) {
+    // Optionally add a timeout or a small delay
+    delay(100);
+    // You can print status for debugging
+    Serial.println("Waiting for Blynk connection...");
+    BlynkEdgent.run();
+  }
+  Serial.println("Blynk is active!");
+  //   WiFi.begin(ssid, password);
+  // WiFi.setSleep(false);
+  // Serial.print("Connecting to ");
+  // Serial.println(ssid);
+  // Serial.print("WiFi connecting");
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
+  // Serial.println("");
+  // Serial.println("WiFi connected");
+  Serial.println();
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the brightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+#if defined(CAMERA_MODEL_ESP32S3_EYE)
+  s->set_vflip(s, 1);
+#endif
+
+// Setup LED FLash if LED pin is defined in camera_pins.h
+#if defined(LED_GPIO_NUM)
+  setupLedFlash(LED_GPIO_NUM);
+#endif
+
+
+
+   
+startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  String ip = WiFi.localIP().toString(); // Get the local IP address as a String
+  String url = "http://" + ip + ":81/stream"; // Concatenate the strings
+
+  Blynk.virtualWrite(V4, url);
+  Serial.print(ip);
+  Serial.println("' to connect");
+  // espClient.setCACert(root_ca);
+  //client.setServer(mqtt_server, mqtt_port);
+  
+  snapshot_buf = (uint8_t*)malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * EI_CAMERA_FRAME_BYTE_SIZE);
+
+    // check if allocation was successful
+    if(snapshot_buf == nullptr) {
+        log_e("ERR: Failed to allocate snapshot buffer!\n");
+       //espres = ESP_FAIL;
+    }
+    fb_mutex= xSemaphoreCreateMutex();
+    frame_queue = xQueueCreate(1, sizeof(uint8_t*)); 
+
+digitalWrite(33,LOW);
+    xTaskCreatePinnedToCore(
+    classifier_task,   // Task function
+    "Detector",     // Name
+    8192,               // Stack size
+    NULL,               // Parameters
+    1,                  // Priority
+    NULL,               // Task handle
+    0                   // Core (0 or 1)
+  );
+  xTaskCreatePinnedToCore(
+    blynk_task,   // Task function
+    "Blynk",     // Name
+    8192,               // Stack size
+    NULL,               // Parameters
+    1,                  // Priority
+    NULL,               // Task handle
+    0                   // Core (0 or 1)
+  );
+  
+
+}
+
+void loop() {
+  delay(10000);
+}
